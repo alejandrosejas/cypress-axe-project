280abfd7721b72f4399577454b191792823886f8
Alejandro's iMac <alejandrosejas@outlook.com>
2021-08-10 17:42:22 -0400

    Added Exclude, onlyTest specific elements + refact

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -1,9 +1,20 @@
 /// <reference types="cypress"/>
 
 describe("Todo application", () => {
-  it("should log any accessibility failures", () => {
+  beforeEach(() => {
     cy.visit("http://todomvc.com/examples/react");
     cy.injectAxe();
+  });
+
+  it("1️⃣ should log any accessibility failures", () => {
     cy.checkA11y();
+  });
+
+  it("2️⃣ should EXCLUDE specific elements from the page", () => {
+    cy.checkA11y({ exclude: [".learn"] });
+  });
+
+  it("3️⃣ should ONLY TEST specific elements from the page", () => {
+    cy.checkA11y(".learn");
   });
 });
